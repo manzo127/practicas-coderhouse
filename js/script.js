73b9cff1c3ce960b2bf56333dad02b960fc67c74
manzo127 <lautaro.ordenes@gmail.com>
2025-08-10 18:34:07 -0300

    haciendo pruebas conceptuales

--- js/script.js
+++ js/script.js
@@ -1,39 +1,8 @@
-function dividir(num1, num2){
-    let div = 0
-    div = num1 / num2
-    return div;
+function suma(num1, num2){
+    let sumador = 0
+    sumador = num1 + num2
+    return sumador;
 }
 
-function sumar(num1, num2){
-    let suma = 0
-    suma = num1 + num2
-    return suma;
-}
-
-function bucle_de_operaciones(){
-    let opcion, num1, num2, divisor = 0, sumador = 0;
-    alert("Saludos ingrese un numero dependiendo de la opcion")
-    opcion = Number(prompt("ingrese una opcion, 1-suma, 2-divicion"))
-    while(opcion < 0){
-        opcion = prompt("ERROR, ingrese una opcion, 1-suma, 2-divicion")
-    }
-    while (opcion > 0){
-        if(opcion == 1){
-            num1 = Number(prompt("ingrese el primer numero"))
-            num2 = Number(prompt("ingrese el segundo numero"))
-            sumador = sumar(num1, num2)
-            console.log("el resultado de la suma es: ",sumador)
-            break;
-        }else if(opcion == 2){
-            num1 = Number(prompt("ingrese el primer numero"))
-            num2 = Number(prompt("ingrese el segundo numero"))
-            divisor = dividir(num1, num2)
-            console.log("el resultado de la divicion es: ",divisor)
-            break;
-        }
-        
-    }
-}
-
-
-bucle_de_operaciones()
+let vari = "suma(1, 2);"
+console.log("el valor es "+ vari)
