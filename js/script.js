e31a7a4f1b86717ae27982d48a37b8185ba7bef7
manzo127 <lautaro.ordenes@gmail.com>
2025-08-16 20:23:38 -0300

    practica de javascript

--- js/script.js
+++ js/script.js
@@ -1,8 +1,59 @@
-function suma(num1, num2){
-    let sumador = 0
-    sumador = num1 + num2
-    return sumador;
+// Tu tarea: Sistema de gestión de estudiantes 
+// 1. Crea un array de objetos estudiantes (nombre, edad, notas[]) 
+// 2. Función para agregar estudiante 
+// 3. Función para calcular promedio de un estudiante 
+// 4. Función para encontrar el mejor estudiante
+
+function carga_notas(list_nota){
+    let notas
+    for (let i = 0; i < 5; i++) {
+        notas = Number(prompt("ingrese unla nota del "+ i + "er cuatrimestre"))
+        while (notas < 0) {
+            notas = Number(prompt("ERROR, ingrese la nota adecuadamente"))
+        }
+        list_nota[i] = notas;
+    }
 }
 
-let vari = "suma(1, 2);"
-console.log("el valor es "+ vari)
+function prom_notas(list_nota){
+    let prom = 0, acum = 0
+    for (let i = 0; i < list_nota.length; i++) {
+        acum += list_nota[i]
+    }
+    prom = acum / list_nota.length
+    return prom;
+}
+function carga_alumnos(alumno,list_nota){
+    let name, edades, notas
+    name = prompt("Ingrese un nombre\n")
+    while (name === "") {
+        name = prompt("ERROR, ingrese un nombre de forma correcta\n")
+    }
+    while (name == "listo"){
+        edades = parseInt(prompt("ingrese la edad"))
+        while(edades < 0 && edades < 12){
+            edades = parseInt(prompt("ERROR, ingrese la edad adecuadamente"))
+        }
+        carga_notas(list_nota)
+        
+        alumno.push({
+            nombre: name,
+            edad: edades,
+            nota: list_nota
+        })
+        
+        
+        name = prompt("Ingrese un nombre\n")
+        while (name === "") {
+            name = prompt("ERROR, ingrese un nombre de forma correcta\n")
+        }
+    }
+}
+
+function mejor_estudiante(list_nota){
+    let prom, cont = 0
+    prom = parseInt(prom_notas(list_nota))
+    if (prom == 7) {
+        cont++
+    }
+}
